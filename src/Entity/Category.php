02f7223e860e59a0e8c62d541ef5b3a3316b1a9b
Cristian Torres <cristian.torres@arcmedia.ch>
2023-03-23 20:54:31 +0100

    Add Category Entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "char", length: 36, unique: true)]
+    private ?string $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $erp_id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $parent = null;
+
+    #[ORM\Column]
+    private ?bool $is_active = null;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getErpId(): ?string
+    {
+        return $this->erp_id;
+    }
+
+    public function setErpId(string $erp_id): self
+    {
+        $this->erp_id = $erp_id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getParent(): ?string
+    {
+        return $this->parent;
+    }
+
+    public function setParent(string $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    public function isIsActive(): ?bool
+    {
+        return $this->is_active;
+    }
+
+    public function setIsActive(bool $is_active): self
+    {
+        $this->is_active = $is_active;
+
+        return $this;
+    }
+}
