3a91ca7962944a3c263235063f88a03b4058687e
Cristian Torres <cristian.torres@arcmedia.ch>
2023-03-24 23:27:02 +0100

    Add Category Infrastructure

---
+++ src/Category/Infrastructure/CategoryPutController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Category\Infrastructure;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class CategoryPutController
+{
+
+}
